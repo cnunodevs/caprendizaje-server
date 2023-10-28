cfd190866c28490453507b5dce5769f9fcf7a4c9
ejramirez <eramirez@extreme.com.co>
2023-10-28 15:12:24 -0500

    Proyecto BASE

---
+++ src/main/java/com/sena/caprenizaje/CaprenizajeApplication.java
@@ -0,0 +1,13 @@
+package com.sena.caprenizaje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaprenizajeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaprenizajeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/caprenizaje/CaprenizajeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.caprenizaje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaprenizajeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
