d6efdbf5f0760deeea682281674d6ded13c4c669
edev404 <eduardoramirezdev@outlook.com>
2023-10-28 17:24:54 -0500

    Constantes rest definidas

---
+++ src/main/java/com/sena/caprenizaje/shared/constant/RestConstant.java
@@ -0,0 +1,20 @@
+package com.sena.caprenizaje.shared.constant;
+
+public class RestConstant {
+
+    public static final String APPLICATION_NAME = "caprendizaje";
+
+    public static class Versioning {
+        public static final String API_VERSION_1 = "api/v1";
+    }
+    
+    public static class Resources {
+        public static final String AUTH_RESOURCE = "auth";
+        public static final String VACANT_RESOURCE = "vacant";
+        public static final String PROFILE_RESOURCE = "profile";
+        public static final String MANAGEMENT_RESOURCE = "management";
+        public static final String POSTULATION_RESOURCE = "postulation";
+        public static final String PERMISSIONS_RESOURCE = "permissions";
+    }
+    
+}
