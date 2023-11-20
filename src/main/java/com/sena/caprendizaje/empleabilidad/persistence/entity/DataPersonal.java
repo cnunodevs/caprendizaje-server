1958c4d19bec436d880d6870670927ad578e3c71
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 10:10:54 -0500

    Update DataPersonal.java

--- src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/DataPersonal.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/DataPersonal.java
@@ -2,10 +2,13 @@ package com.sena.caprendizaje.empleabilidad.persistence.entity;
 
 import com.sena.caprendizaje.shared.model.entity.GenericEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,5 +25,28 @@ public class DataPersonal implements GenericEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "telefono")
+    private String telefono;
+
+    @Column(name = "documento")
+    private String documento;
+
+    @ManyToOne
+    @JoinColumn(name = "profesion")
+    private Parametro profesion;
+
+    @ManyToOne
+    @JoinColumn(name = "tipo_documento")
+    private Parametro tipoDocumento;
+
+    private String curriculum;
+
     
 }
