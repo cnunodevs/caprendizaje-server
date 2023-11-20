0b99f95fa24d70fefbc441ae992c343b2d955219
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 11:52:50 -0500

    Update DataPersonalMapper.java

--- src/main/java/com/sena/caprendizaje/empleabilidad/model/mapper/DataPersonalMapper.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/model/mapper/DataPersonalMapper.java
@@ -6,19 +6,33 @@ import com.sena.caprendizaje.empleabilidad.model.dto.DataPersonalModel;
 import com.sena.caprendizaje.empleabilidad.persistence.entity.DataPersonal;
 import com.sena.caprendizaje.shared.model.mapper.GenericMapper;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class DataPersonalMapper implements GenericMapper<DataPersonalModel, DataPersonal> {
+
+    private final ArchivoMapper archivoMapper;
+    private final ParametroMapper parametroMapper;
 
     @Override
     public DataPersonalModel mapToModel(DataPersonal entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mapToModel'");
+        return DataPersonalModel.builder().id(entity.getId()).descripcion(entity.getDescripcion())
+                .email(entity.getEmail()).telefono(entity.getTelefono()).documento(entity.getDocumento())
+                .profesion(entity.getProfesion() != null ? parametroMapper.mapToModel(entity.getProfesion()) : null)
+                .tipoDocumento(entity.getTipoDocumento() != null ? parametroMapper.mapToModel(entity.getTipoDocumento()) : null)
+                .curriculum(entity.getCurriculum() != null ? archivoMapper.mapToModel(entity.getCurriculum()) : null)
+                .build();
     }
 
     @Override
     public DataPersonal mapToEntity(DataPersonalModel model) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mapToEntity'");
+        return DataPersonal.builder().id(model.getId()).descripcion(model.getDescripcion())
+                .email(model.getEmail()).telefono(model.getTelefono()).documento(model.getDocumento())
+                .profesion(model.getProfesion() != null ? parametroMapper.mapToEntity(model.getProfesion()) : null)
+                .tipoDocumento(model.getTipoDocumento() != null ? parametroMapper.mapToEntity(model.getTipoDocumento()) : null)
+                .curriculum(model.getCurriculum() != null ? archivoMapper.mapToEntity(model.getCurriculum()) : null)
+                .build();
     }
-    
+
 }
