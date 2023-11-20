839878a26ce7419a1d015716fad2fae3cec79543
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 12:01:50 -0500

    Update ParametroMapper.java

--- src/main/java/com/sena/caprendizaje/empleabilidad/model/mapper/ParametroMapper.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/model/mapper/ParametroMapper.java
@@ -6,19 +6,30 @@ import com.sena.caprendizaje.empleabilidad.model.dto.ParametroModel;
 import com.sena.caprendizaje.empleabilidad.persistence.entity.Parametro;
 import com.sena.caprendizaje.shared.model.mapper.GenericMapper;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class ParametroMapper implements GenericMapper<ParametroModel, Parametro> {
+
+    private final MaestroParametroMapper maestroParametroMapper;
 
     @Override
     public ParametroModel mapToModel(Parametro entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mapToModel'");
+        return ParametroModel.builder().id(entity.getId()).nombre(entity.getNombre()).codigo(entity.getCodigo())
+                .maestroParametro(entity.getMaestroParametro() != null
+                        ? maestroParametroMapper.mapToModel(entity.getMaestroParametro())
+                        : null)
+                .activo(entity.getActivo()).build();
     }
 
     @Override
     public Parametro mapToEntity(ParametroModel model) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mapToEntity'");
+        return Parametro.builder().id(model.getId()).nombre(model.getNombre()).codigo(model.getCodigo())
+                .maestroParametro(model.getMaestroParametro() != null
+                        ? maestroParametroMapper.mapToEntity(model.getMaestroParametro())
+                        : null)
+                .activo(model.getActivo()).build();
     }
-    
+
 }
