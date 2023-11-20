e06ec205d7fe1edda4b60929821b4650806777ac
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 10:38:19 -0500

    Update Vacante.java

--- src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Vacante.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Vacante.java
@@ -1,11 +1,18 @@
 package com.sena.caprendizaje.empleabilidad.persistence.entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import com.sena.caprendizaje.shared.model.entity.GenericEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,5 +29,40 @@ public class Vacante implements GenericEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "salario")
+    private Double salario;
+    
+    @ManyToOne
+    @JoinColumn(name = "moneda")
+    private Parametro moneda;
+
+    @ManyToOne
+    @JoinColumn(name = "empresa")
+    private Empresa empresa;
+
+    @ManyToOne
+    @JoinColumn(name = "ciudad")
+    private Parametro ciudad;
+
+    @ManyToOne
+    @JoinColumn(name = "modalidad")
+    private Parametro modalidad;
+
+    @ManyToOne
+    @JoinColumn(name = "categoria")
+    private Parametro categoria;
+
+    @Column(name = "activo")
+    private boolean activo;
+
+    @Column(name = "fecha_creacion")
+    @CreationTimestamp
+    private LocalDateTime fechaCreacion;
+
+    @Column(name = "ultima_actualizacion")
+    @CreationTimestamp
+    private LocalDateTime ultimaActualizacion;
+    
     
 }
