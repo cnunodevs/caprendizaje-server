a724f81b7079bc88cf1bf0e036638e119ac6fb07
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 10:46:26 -0500

    Create Empresa.java

---
+++ src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Empresa.java
@@ -0,0 +1,52 @@
+package com.sena.caprendizaje.empleabilidad.persistence.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import com.sena.caprendizaje.shared.model.entity.GenericEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor @NoArgsConstructor
+@Table(name = "empresas", schema = "public")
+public class Empresa implements GenericEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "codigo")
+    private String codigo;
+
+    @OneToOne
+    @JoinColumn(name = "administrador")
+    private Usuario administrador;
+
+    @ManyToOne
+    @JoinColumn(name = "foto_perfil")
+    private Archivo fotoPerfil;
+
+    @CreationTimestamp
+    @Column(name = "fecha_creacion")
+    private LocalDateTime fechaCreacion;
+    
+}
