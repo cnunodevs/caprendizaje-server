fb95ab1887ace3621f1cef89e25799678a313596
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 12:01:47 -0500

    Update MaestroParametroMapper.java

--- src/main/java/com/sena/caprendizaje/empleabilidad/model/mapper/MaestroParametroMapper.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/model/mapper/MaestroParametroMapper.java
@@ -6,19 +6,20 @@ import com.sena.caprendizaje.empleabilidad.model.dto.MaestroParametroModel;
 import com.sena.caprendizaje.empleabilidad.persistence.entity.MaestroParametro;
 import com.sena.caprendizaje.shared.model.mapper.GenericMapper;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class MaestroParametroMapper implements GenericMapper<MaestroParametroModel, MaestroParametro> {
 
     @Override
     public MaestroParametroModel mapToModel(MaestroParametro entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mapToModel'");
+        return MaestroParametroModel.builder().id(entity.getId()).nombre(entity.getNombre()).codigo(entity.getCodigo()).activo(entity.getActivo()).build();
     }
 
     @Override
     public MaestroParametro mapToEntity(MaestroParametroModel model) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mapToEntity'");
+        return MaestroParametro.builder().id(model.getId()).nombre(model.getNombre()).codigo(model.getCodigo()).activo(model.getActivo()).build();
     }
     
 }
