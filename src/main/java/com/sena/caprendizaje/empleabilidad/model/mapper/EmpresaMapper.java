343de8707b8ba45b7fc9d6a51a7f10253b323f92
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 11:56:46 -0500

    Update EmpresaMapper.java

--- src/main/java/com/sena/caprendizaje/empleabilidad/model/mapper/EmpresaMapper.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/model/mapper/EmpresaMapper.java
@@ -6,19 +6,29 @@ import com.sena.caprendizaje.empleabilidad.model.dto.EmpresaModel;
 import com.sena.caprendizaje.empleabilidad.persistence.entity.Empresa;
 import com.sena.caprendizaje.shared.model.mapper.GenericMapper;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class EmpresaMapper implements GenericMapper<EmpresaModel, Empresa> {
+
+    private final ArchivoMapper archivoMapper;
+    private final UsuarioMapper usuarioMapper;
 
     @Override
     public EmpresaModel mapToModel(Empresa entity) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mapToModel'");
+        return EmpresaModel.builder().id(entity.getId()).nombre(entity.getNombre()).codigo(entity.getCodigo())
+                .administrador(entity.getAdministrador() != null ? usuarioMapper.mapToModel(entity.getAdministrador()) : null)
+                .fotoPerfil(entity.getFotoPerfil() != null ? archivoMapper.mapToModel(entity.getFotoPerfil()) : null)
+                .build();
     }
 
     @Override
     public Empresa mapToEntity(EmpresaModel model) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'mapToEntity'");
+        return Empresa.builder().id(model.getId()).nombre(model.getNombre()).codigo(model.getCodigo())
+                .administrador(model.getAdministrador() != null ? usuarioMapper.mapToEntity(model.getAdministrador()) : null)
+                .fotoPerfil(model.getFotoPerfil() != null ? archivoMapper.mapToEntity(model.getFotoPerfil()) : null)
+                .build();
     }
-    
+
 }
