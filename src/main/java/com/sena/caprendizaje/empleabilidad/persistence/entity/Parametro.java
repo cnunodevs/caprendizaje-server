be46e76c51f4defa1bbce2b69784fe1629df26f4
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 10:51:42 -0500

    Update Parametro.java

--- src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Parametro.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Parametro.java
@@ -2,10 +2,13 @@ package com.sena.caprendizaje.empleabilidad.persistence.entity;
 
 import com.sena.caprendizaje.shared.model.entity.GenericEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -22,5 +25,18 @@ public class Parametro implements GenericEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "codigo")
+    private String codigo;
+
+    @ManyToOne
+    @JoinColumn(name = "maestro")
+    private MaestroParametro maestroParametro;
+
+    @Column(name = "activo")
+    private boolean activo;
     
 }
