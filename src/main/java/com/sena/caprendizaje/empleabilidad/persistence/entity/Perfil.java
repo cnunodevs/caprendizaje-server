c3d4d6d4c53e5ed566b663569cbfca3ba7ef94f7
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 10:53:43 -0500

    Update Perfil.java

--- src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Perfil.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Perfil.java
@@ -2,6 +2,7 @@ package com.sena.caprendizaje.empleabilidad.persistence.entity;
 
 import com.sena.caprendizaje.shared.model.entity.GenericEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,5 +23,17 @@ public class Perfil implements GenericEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "descripcion")
+    private String desripcion;
+
+    @Column(name = "codigo")
+    private String codigo;
+
+    @Column(name = "activo")
+    private boolean activo;
     
 }
