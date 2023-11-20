ba1f8752fd087e8a2aa1db6ac4cb6ce8199a2ccd
edev404 <eduardoramirezdev@outlook.com>
2023-11-20 11:00:20 -0500

    Update Usuario.java

--- src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Usuario.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Usuario.java
@@ -1,11 +1,18 @@
 package com.sena.caprendizaje.empleabilidad.persistence.entity;
 
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import com.sena.caprendizaje.shared.model.entity.GenericEntity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,5 +30,25 @@ public class Usuario implements GenericEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @JoinColumn(name = "datos_personal")
+    private DataPersonal datosPersonales;
+
+    @Column(name = "usuario")
+    private String usuario;
+
+    @Column(name = "contrasena")
+    private String contraseña;
+
+    @ManyToOne
+    @JoinColumn(name = "perfil")
+    private Perfil perfil;
+
+    @Column(name = "activo")
+    private boolean activo;
+
+    @Column(name = "fecha_creacion")
+    @CreationTimestamp
+    private LocalDateTime fechaCreacion;
 
 }
