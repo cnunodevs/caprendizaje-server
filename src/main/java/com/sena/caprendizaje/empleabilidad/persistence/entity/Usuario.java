5aed038774b9ef2c40092180eb1802a6cd9744c4
edev404 <eduardoramirezdev@outlook.com>
2023-11-25 18:58:14 -0500

    Entidades actualizadas

--- src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Usuario.java
+++ src/main/java/com/sena/caprendizaje/empleabilidad/persistence/entity/Usuario.java
@@ -9,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -27,6 +28,7 @@ public class Usuario implements GenericEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @OneToOne
     @JoinColumn(name = "datos_personal")
     private DataPersonal datosPersonales;
 
